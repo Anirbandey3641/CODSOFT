ad9e44f267bd0ac588fdd8d1d0dc413b7e0c369b
Anirbandey3641 <dey364192@gmail.com>
2026-01-13 11:06:44 +0530

    Delete c directory

--- c/simple_interest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float si,r,t,p;
-    printf("enter the principal amount,rate of interest and time");
-    scanf("%f%f%f",&p,&r,&t);
-    si=(p*r*t)/100;
-    printf("the simple interest is %f",si);
-    return 0;
-}
--- c/temp.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-    float c,f;
-    printf("enter the temperature in degree celcius");
-    scanf("%f",&c);
-    f=(c*1.8)+32;
-    printf("the temperature farenheit :%f",f);  
-    getch();
-}
